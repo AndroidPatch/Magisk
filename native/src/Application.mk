929dc9f6b3fb302d8b049af7cf77bdda8735a344
GarfieldHan <2652609017@qq.com>
2024-10-14 11:16:56 +0800

    native: Skip tests, more optimizations, build magiskpolicy and resetprop only
    Signed-off-by: GarfieldHan <2652609017@qq.com>

--- native/src/Application.mk
+++ native/src/Application.mk
@@ -1,10 +1,10 @@
 APP_BUILD_SCRIPT := src/Android.mk
-APP_CFLAGS       := -Wall -Oz -fomit-frame-pointer
-APP_CPPFLAGS     := -std=c++23
+APP_CFLAGS       := -Wall -O3 -fomit-frame-pointer -fdata-sections -ffunction-sections -fvisibility=hidden -fvisibility-inlines-hidden -funwind-tables -fstack-protector-strong -fno-exceptions -fno-rtti -fno-unwind-tables -fno-asynchronous-unwind-tables -fno-stack-protector -fno-semantic-interposition -U_FORTIFY_SOURCE
+APP_CPPFLAGS     := -std=c++23 -O3 -fdata-sections -ffunction-sections -fvisibility=hidden -fvisibility-inlines-hidden -funwind-tables -fstack-protector-strong -fno-exceptions -fno-rtti -fno-unwind-tables -fno-asynchronous-unwind-tables -fno-stack-protector -fno-semantic-interposition -U_FORTIFY_SOURCE
 APP_STL          := none
 APP_PLATFORM     := android-23
 APP_THIN_ARCHIVE := true
-APP_STRIP_MODE   := none
+APP_STRIP_MODE   := --strip-all
 
 ifdef MAGISK_DEBUG
 
@@ -15,8 +15,8 @@ APP_LDFLAGS      += -flto=thin
 else
 
 NDK_APP_OUT 	 := ./obj/release
-APP_CFLAGS       += -flto
-APP_LDFLAGS      += -flto -Wl,--icf=all
+APP_CFLAGS       += -flto -DNDEBUG -O3
+APP_LDFLAGS      += -flto -Wl,--icf=all,--lto-O3,-s,-x,--gc-sections,--no-undefined
 
 endif
 
